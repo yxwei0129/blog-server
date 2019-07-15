615520279e884f3db46c07f81d5cb510e5473dd1
yxwei0129 <745780509@qq.com>
2019-07-15 23:54:07 +0800

    实现分页查询

--- service/article.js
+++ service/article.js
@@ -2,25 +2,30 @@
  * created by Yuxin Wei on 2019/7/13
  * 获取文章
  */
+const JsonWrite = require('../util/output');
 const Article = require('../model/article');
 
 //博客服务层
 const ArticleService = {
 
     /**
-     * 获取所有博客列表
+     * 获取所有博客列表,按页查询
+     * @param start 起始
+     * @param limit 限制
      */
-    getArticleList: function () {
+    getArticleList: function (start, limit) {
         return new Promise(function (resolve, reject) {
-            Article.findAll().then(function (project) {
-                if (project) {
-                    resolve(project);
-                } else {
-                    reject({
-                        code: '1001',
-                        info: '没有博客'
-                    })
+            Article.findAll(
+                {
+                    limit: limit,
+                    offset: start * limit
                 }
+            ).then(function (project) {
+                if (project) {
+                    resolve(JsonWrite.success(project));
+                }
+            }, function (reason) {
+                reject(JsonWrite.error(reason))
             });
         });
 
