92f1b4ee16c731667c69c2d80ca29350d0fe6bef
yxwei0129 <745780509@qq.com>
2019-07-17 20:56:06 +0800

    博客detail增加逻辑删除

--- model/detail.js
+++ model/detail.js
@@ -3,10 +3,32 @@
  * 博客详情
  */
 var mongoose = require('../core/mongodb');
+var moment = require('moment');
+var co = require('co')
 
 var BlogSchema = new mongoose.Schema({
-    id: String,
-    name: String,
-    html: String
-});
-module.exports = BlogSchema;
+        articleId: {  //博客id
+            type: String,
+            required: true
+        },
+        blogDetail: {
+            type: String,
+            required: true
+        },
+        deleteFlag: {   //逻辑删除
+            type: Boolean,
+            default: 0
+        },
+        insertTime: { // 插入时间
+            type: Date,
+            default: Date.now,
+            get: function (time) {
+                return moment(time).format('YYYY-MM-DD')
+            }
+        }
+    })
+;
+
+var BlogDetail = mongoose.model('BlogDetail', BlogSchema);
+
+module.exports = BlogDetail;
