eab83b2af66b2ed60c1f2f5a1c699cca2502211c
yxwei0129 <745780509@qq.com>
2019-07-20 22:36:54 +0800

    login接口

--- routes/login.js
+++ routes/login.js
@@ -1,3 +1,27 @@
 /**
  * created by Yuxin Wei on 2019/7/20
+ * login
  */
+
+var express = require('express');
+var router = express.Router();
+const LoginService = require('../service/login');
+
+const JsonWrite = require('../util/output');
+
+router.get('/login', function (req, res) {
+
+    var loginName = req.query.loginName;
+    var password = req.query.password;
+
+    LoginService.login(loginName, password).then(function (value) {
+        res.send(value)
+    })
+});
+
+router.get('/logout', function (req, res) {
+    res.send('logout')
+})
+
+
+module.exports = router;
