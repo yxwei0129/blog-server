1e46d123bb6bf7d5646f3b51dc4253a1999bc429
yxwei0129 <745780509@qq.com>
2019-07-23 22:15:35 +0800

    添加t路由

--- routes/login.js
+++ routes/login.js
@@ -7,19 +7,21 @@ var express = require('express');
 var router = express.Router();
 const LoginService = require('../service/login');
 
-const JsonWrite = require('../util/jsonSeq');
+const jsonSeq = require('../util/jsonSeq');
 
 router.get('/login', function (req, res) {
     var loginName = req.query.loginName;
     var password = req.query.password;
-
+    if (loginName === '' || password === '') {
+        res.send(jsonSeq.success('SH-1001', '', '用户名或密码为空!'))
+    }
     LoginService.login(loginName, password).then(function (value) {
         res.send(value)
     })
 });
 
 router.get('/logout', function (req, res) {
-    res.send({data:Math.random()})
+    res.send({data: Math.random()})
 })
 
 
